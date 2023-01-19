6e729d0a8fa94389dc42a51b7102e3049e06c01d
Ashish Kumar <ashishbhagat330@gmail.com>
2023-01-20 00:05:21 +0530

    added

---
+++ CourseMonitoringSystem/src/com/masai/BatchUsecases/deleteBatchUseCase.java
@@ -0,0 +1,42 @@
+package com.masai.BatchUsecases;
+
+import java.util.Scanner;
+
+import com.masai.dao.AdminDao;
+import com.masai.dao.AdminDaoImpl;
+import com.masai.exception.BatchException;
+
+public class deleteBatchUseCase {
+
+	public static void main(String[] args) {
+		System.out.println("Enter Name To Delete Batch");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		 Scanner sc=new Scanner(System.in);      
+		 AdminDao dao=new AdminDaoImpl();
+		 while(true) {
+			 System.out.println("Enter name of Batch");
+				String name=sc.next();
+				try {
+					String result=dao.deleteBatchByName(name);
+					System.out.println(result);
+				} catch (BatchException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println("Do You want to Delete More Batch?(Y/N)");
+				String res=sc.next();
+				if(res.equalsIgnoreCase("N")) {
+					System.out.println("Your Batch is deleted");
+					break;
+				}
+				
+		 }
+
+	}
+
+}
---
+++ CourseMonitoringSystem/src/com/masai/FacultyUsecases/createfacultyUseCases.java
@@ -0,0 +1,63 @@
+package com.masai.FacultyUsecases;
+
+import java.util.Scanner;
+
+import com.masai.dao.AdminDao;
+import com.masai.dao.AdminDaoImpl;
+import com.masai.exception.FacultyException;
+import com.masai.model.Faculty;
+
+public class createfacultyUseCases {
+
+	public static void main(String[] args) {
+		System.out.println("Enter Faculty Details");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace( );
+		}
+       Scanner sc=new Scanner(System.in);      
+		
+		AdminDao dao=new AdminDaoImpl();
+		while(true) {
+			System.out.println("Enter name of Faculty");
+			String name=sc.next();
+			System.out.println("Enter address of Faculty");
+			String ads=sc.next();
+			System.out.println("Enter MobileNo of Faculty");
+			String mob=sc.next();
+			System.out.println("Enter email of Faculty");
+			String emails=sc.next();
+			System.out.println("Enter Password of Faculty");
+			String pass=sc.next();
+			Faculty fac=new Faculty();
+			fac.setFname(name);
+			fac.setAddress(ads);
+			fac.setEmail(emails);
+			fac.setPassword(pass);
+			fac.setMobile(mob);
+			try {
+				String result=dao.createFaculty(fac);
+				System.out.println(result);
+			} catch (FacultyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("Do You want to Register More Faculty?(Y/N)");
+			String res=sc.next();
+			if(res.equalsIgnoreCase("N")) {
+				System.out.println("Thank You for Faculty Registration");
+				break;
+			
+			
+			
+		}
+		
+		
+		
+
+	}
+
+	}
+}
